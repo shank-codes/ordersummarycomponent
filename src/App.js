b46aff48e7bbe8fae28229d846916061ff61732a
Shashank <shashankhiremat22@gmail.com>
2023-05-19 18:24:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img className="heroImage" src="/images/illustration-hero.svg" alt="illustration-hero" />
+      <div className="content">
+      <p className="title">Order Summary</p>
+      <p className="description">
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhere you like!
+      </p>
+      <div className="priceSection">
+        <img src="/images/icon-music.svg" alt="music-icon" />
+        <div className="price">
+          <span className="priceTitle">Annual Plan</span>
+          <span className="subscription">$59.99/year</span>
+        </div>
+        <span className="changeText">Change</span>
+      </div>
+      <button className="btn btnText">Proceed to Payment</button>
+      <p className="cancelText">Cancel Order</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+Order Summary
+
+  You can now listen to millions of songs, audiobooks, and podcasts on any 
+  device anywhere you like!
+
+  Annual Plan
+  $59.99/year
+
+  Change
+
+  Proceed to Payment
+  Cancel Order
+*/
